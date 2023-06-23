10d28ae1ee1c5b80d8f5a745dc36ff4d3be8cbf9
PitBosch <pietro.boschini97@gmail.com>
2023-06-23 11:19:16 +0200

    plotly

--- createpages.py
+++ createpages.py
@@ -14,7 +14,7 @@ for json_file in json_files:
     # Add title, subtitle, and content to the Markdown file
     page_title = f'{filename}'
     page_subtitle = f'Come si dice {filename}?'
-    page_content = f'{{{{ if .Params.plotly }}}}\n{{<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>}}\n{{ end }}{{{{< plotly json="/plotly/{json_file}" >}}}}'
+    page_content = f'{{{{< load-plotly >}}}}\n{{{{< plotly json="/plotly/{json_file}" >}}}}'
 
     with open(page_file, 'w') as f:
         f.write(f'---\ntitle: "{page_title}"\n---\n\n## {page_subtitle}\n\n{page_content}')
